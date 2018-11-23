57470c45875acf0a59fdd60439aa84431adb53a9
srijananand <you@example.com>
2018-11-23 15:31:13 +0530

    signup_serializer_complete

---
+++ accounts/serializers.py
@@ -0,0 +1,73 @@
+import datetime
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
+expire_delta = api_settings.JWT_REFRESH_EXPIRATION_DELTA
+
+User = get_user_model()
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    token = serializers.SerializerMethodField(read_only=True)
+    expires = serializers.SerializerMethodField(read_only=True)
+    message = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'password2',
+            'token',
+            'expires',
+            'message'
+        ]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def get_message(self):
+        return "User created Successfully. Email verification required"
+
+    def get_expires(self, user):
+        return timezone.now() + expire_delta - datetime.timedelta(seconds=200)
+
+    def validate_email(self, value):
+        qs = User.objects.filter(email__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError("User with this email already exists")
+        return value
+
+    def validate_username(self, value):
+        qs = User.objects.filter(username__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError("User with this email already exists")
+        return value
+
+    def get_token(self, user):
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        return token
+
+    def validate(self, data):
+        pw = data.get('password')
+        pw2 = data.pop('password2')
+        if pw != pw2:
+            raise serializers.ValidationError("Passwords must match")
+        return data
+
+    def create(self, validated_data):
+        user_obj = User.objects.create(
+            username=validated_data.get('username'),
+            email=validated_data.get('email')
+        )
+        user_obj.set_password(validated_data.get('password'))
+        user_obj.is_active = False
+        user_obj.save()
+        return user_obj
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from accounts.views import *
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
+    path('signup/', SignupView.as_view()),
 ]
